52648c04c31d4a8ca95cf322274a2a2c78b5907a
eltx1 <info.eltx@gmail.com>
2025-12-12 16:49:37 +0200

    Add Google Analytics snippet to footers

--- app/(site)/components/Footer.tsx
+++ app/(site)/components/Footer.tsx
@@ -1,13 +1,26 @@
 import Link from 'next/link';
+import Script from 'next/script';
 
 export default function Footer() {
   return (
-    <footer className="mt-10 border-t border-white/5 p-4 text-center text-xs text-white/60">
-      <div className="space-x-4">
-        <Link href="/" className="hover:text-white">ELTX</Link>
-        <Link href="/faq" className="hover:text-white">FAQ</Link>
-      </div>
-      <div className="mt-2">© {new Date().getFullYear()} ELTX</div>
-    </footer>
+    <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-N82B7F9S45" strategy="afterInteractive" />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-N82B7F9S45');
+        `}
+      </Script>
+      <footer className="mt-10 border-t border-white/5 p-4 text-center text-xs text-white/60">
+        <div className="space-x-4">
+          <Link href="/" className="hover:text-white">ELTX</Link>
+          <Link href="/faq" className="hover:text-white">FAQ</Link>
+        </div>
+        <div className="mt-2">© {new Date().getFullYear()} ELTX</div>
+      </footer>
+    </>
   );
 }
