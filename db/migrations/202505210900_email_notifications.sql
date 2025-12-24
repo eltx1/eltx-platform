618537ebb839bbf88b99f8b7314b0d92567f62c9
eltx1 <info.eltx@gmail.com>
2025-12-24 15:11:58 +0200

    Add P2P email notifications with admin controls

--- db/migrations/202505210900_email_notifications.sql
+++ db/migrations/202505210900_email_notifications.sql
@@ -4,4 +4,6 @@ INSERT IGNORE INTO platform_settings (name, value) VALUES
   ('email_admin_recipients', ''),
   ('email_user_welcome_enabled', '1'),
   ('email_user_kyc_enabled', '1'),
-  ('email_admin_kyc_enabled', '1');
+  ('email_admin_kyc_enabled', '1'),
+  ('email_user_p2p_enabled', '1'),
+  ('email_admin_p2p_enabled', '1');
